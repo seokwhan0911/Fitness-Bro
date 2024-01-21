292bd52cba8041860e0a4d942e53730f1cbeb044
dayeong <dayeongyun184@gmail.com>
2024-01-21 12:39:59 +0900

    api 구현

---
+++ src/main/java/FitnessBro/apiPayload/Utill/ValidationUtils.java
@@ -0,0 +1,17 @@
+package com.intp.common.util;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ValidationUtils {
+    public static Map<String, String> getValidationErrors(Errors errors) {
+        return errors.getAllErrors().stream()
+                .collect(Collectors.toMap(
+                        error -> ((FieldError) error).getField(),
+                        error -> error.getDefaultMessage()
+                ));
+    }
+}

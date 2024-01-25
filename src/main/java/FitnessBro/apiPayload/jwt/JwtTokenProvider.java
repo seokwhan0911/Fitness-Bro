ce1824eed35ae4f4b0ec9bde78921599de7015ac
Yoon-Jemin <yjm1800@naver.com>
2024-01-25 17:59:17 +0900

    jwt 오류 제거

--- src/main/java/FitnessBro/apiPayload/jwt/JwtTokenProvider.java
+++ src/main/java/FitnessBro/apiPayload/jwt/JwtTokenProvider.java
@@ -3,12 +3,6 @@ package FitnessBro.apiPayload.jwt;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;

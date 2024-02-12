2e7b670f99260b4f586a716a994912f0a2ffec0f
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-02-12 19:20:35 +0900

    주석없앰

--- src/main/java/FitnessBro/apiPayload/configuration/StompWebSocketConfig.java
+++ src/main/java/FitnessBro/apiPayload/configuration/StompWebSocketConfig.java
@@ -3,8 +3,9 @@ package FitnessBro.apiPayload.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 
@@ -19,7 +20,6 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .withSockJS();
     }
 
-    /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //to server

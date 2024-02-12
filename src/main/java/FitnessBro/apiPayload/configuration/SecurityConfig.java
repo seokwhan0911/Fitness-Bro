553a3d942388a0b6dcb834cbe38aad8887044330
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-02-12 19:38:48 +0900

    permitAll추가

--- src/main/java/FitnessBro/apiPayload/configuration/SecurityConfig.java
+++ src/main/java/FitnessBro/apiPayload/configuration/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -44,6 +43,7 @@ public class SecurityConfig {
                                 .requestMatchers("/coaches/sign-up").permitAll()
                                 .requestMatchers("/coaches/login").permitAll()
                                 .requestMatchers("/login/oauth2/code/**").permitAll()
+                                .requestMatchers("/**").permitAll()
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
 

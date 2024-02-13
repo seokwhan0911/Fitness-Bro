3478f51814b48dd49f1800f1f4a521813cee59a1
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-02-13 21:07:40 +0900

    cors부분 로컬호스트3000추가 크리덴셜 트루 추가

--- src/main/java/FitnessBro/apiPayload/configuration/SecurityConfig.java
+++ src/main/java/FitnessBro/apiPayload/configuration/SecurityConfig.java
@@ -65,6 +65,8 @@ public class SecurityConfig {
         corsConfiguration.addAllowedMethod("PUT");
         corsConfiguration.addAllowedMethod("PATCH"); // Add PATCH method
         corsConfiguration.addAllowedMethod("DELETE");
+        corsConfiguration.addAllowedOrigin("http://localhost3000");
+        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedHeader("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

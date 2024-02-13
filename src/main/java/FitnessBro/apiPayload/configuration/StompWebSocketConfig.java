b6c6ebee6c48b82a726ac4e7f82dcb8e1a4f51d5
Seokhwan Choi <seokwhan123@g.hongik.ac.kr>
2024-02-13 19:46:33 +0900

    setAllowedOrigins -> setAllowdOriginsPatterns 웹소켓

--- src/main/java/FitnessBro/apiPayload/configuration/StompWebSocketConfig.java
+++ src/main/java/FitnessBro/apiPayload/configuration/StompWebSocketConfig.java
@@ -3,8 +3,9 @@ package FitnessBro.apiPayload.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 
@@ -15,7 +16,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")  // js에서 웹소켓 접속시 ex) var socket = new SockJS("/stomp/chat")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 

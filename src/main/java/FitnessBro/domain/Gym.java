cbe4602a8d26b91cb02d2dba99164ad29f36237e
BangProx <stush0626@gmail.com>
2024-02-15 13:48:23 +0900

    gym entity 수정

--- src/main/java/FitnessBro/domain/Gym.java
+++ src/main/java/FitnessBro/domain/Gym.java
@@ -15,6 +15,18 @@ public class Gym extends BaseEntity {
     @GeneratedValue
     @Column(name = "gym_id")
     private Long id;
-
+    //소재지전화
+    private String phoneNumber;
+    //도로명전체주소
     private String address;
+    //도로명우편번호
+    private String postalCode;
+    //사업장명
+    private String name;
+    //시
+    private String region;
+    //구
+    private String subAddress;
+    //도로명
+    private String detailAddress;
 }

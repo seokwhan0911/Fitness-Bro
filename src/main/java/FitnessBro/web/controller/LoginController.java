729e2a9a934062e45b38a13ac62bfa24173b6a80
chris <aliveck0208@gmail.com>
2024-02-18 22:28:44 +0900

    카카오 로그인 컨트롤러 변경

--- src/main/java/FitnessBro/web/controller/LoginController.java
+++ src/main/java/FitnessBro/web/controller/LoginController.java
@@ -48,7 +48,15 @@ public class LoginController {
 
 
     @GetMapping("/oauth2/code/kakao")
-    public ResponseEntity<ApiResponse<LoginDTO>> KakaoLogin(@RequestParam("code") String code) {
+    public ResponseEntity<ApiResponse<LoginDTO>> KakaoCode(@RequestParam("code") String code) {
+
+
+
+        return null;
+    }
+
+    @PostMapping("/oauth2/code/kakao")
+    public ResponseEntity<ApiResponse<LoginDTO>> KakaoLogin(@RequestBody String code) {
 
         ResponseEntity<String> stringResponseEntity = kakaoService.getKakaoAccessToken(code);
 
